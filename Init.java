ee43f6967df9e28dc2aaa42dfa1398bc9439f92b
Rithwik <vedpathakrithwik@gmail.com>
2021-07-31 08:32:12 +0530

    Init program added

---
+++ Init.java
@@ -0,0 +1,22 @@
+public class Init {
+    {
+        System.out.println("First INIT");
+    }
+
+    Init() {
+        System.out.println("Inside default constructor");
+    }
+
+    Init(int x) {
+        this();
+        System.out.println("Inside one parameter constructor");
+    }
+
+    {
+        System.out.println("Second INIT");
+    }
+
+    public static void main(String[] args) {
+        Init i = new Init(4);
+    }
+}
